d2c51031bb859a19ab8bb4817f604cf66eb78599
James <jamesghill@yahoo.co.uk>
2015-02-02 20:47:36 +0000

    Completed.

--- BBK - PiJ/src/Lesson_16_3_cat_a.java
+++ BBK - PiJ/src/Lesson_16_3_cat_a.java
@@ -8,24 +8,20 @@ public class Lesson_16_3_cat_a {
             String name = "./" + args[0];
             File file = new File(name);
             BufferedReader in = null;
-        
-            if(file.exists()){
-                try {
-                    in = new BufferedReader(new FileReader(file));
-                    String line;
-                    while((line = in.readLine()) != null){
-                        System.out.println(line);
-                    }
-                } catch (FileNotFoundException ex){
-                    System.out.println("File " + file + " does not exist.");
-                } catch (IOException ex){
-                    ex.printStackTrace();
-                } finally {
-                    closeReader(in);
+            
+            try {
+                in = new BufferedReader(new FileReader(file));
+                String line;
+                while((line = in.readLine()) != null){
+                    System.out.println(line);
                 }
+            } catch (FileNotFoundException ex){
+                System.out.println("File " + name + " does not exist.");
+            } catch (IOException ex){
+                ex.printStackTrace();
+            } finally {
+                closeReader(in);
             }
-        } else {
-            System.out.println("No file name provided.");
         }
     }
     
